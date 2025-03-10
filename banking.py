52a32ddceaa3687a2355da2a8dae3b9814f75212
rubabdran <rtbdrani@gmail.com>
2025-03-10 09:40:41 +0300

    update

--- banking.py
+++ banking.py
@@ -1,35 +1,107 @@
 import csv 
 
-class bankAccount:
-    def __init__(self,account_id,first_name,last_name,password):
-        self.account_id=account_id
-        self.first_name=first_name
-        self.last_name=last_name
-        self.password=password
+class bank:
+     
+    def __init__(self):
+        self.is_logged_in = False  
+        self.logged_in_user_id = ""
+        self.fieldnames = ['account_id', 'first_name', 'last_name', 'password', 'balance_checking', 'balance_savings']
+
+    def add_account(self):
+        account_id = input("Enter your ID: ")
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        password = input("Enter password: ")
+        
     
-    def addAccount(self, account_id, first_name, last_name, password, balance_checking=0, balance_savings=0):
-         fieldnames = ['account_id', 'first_name', 'last_name', 'password', 'balance_checking', 'balance_savings']
-         
-         with open('bank.csv', "r", newline="") as file:
+        account_type = input("Choose account type: (1) Checking (2) Savings (3) Both: ")
+        if account_type == "1":
+            balance_checking = 0
+            balance_savings = "Null"
+                
+        elif account_type == "2":
+            balance_checking = "Null"
+            balance_savings = 0
+                
+        elif account_type == "3":
+            balance_checking = 0
+            balance_savings = 0
+               
+                
+        with open('bank.csv', "r", newline="") as file:
             contents = csv.DictReader(file)
             for row in contents:
-                if row["account_id"] == str(account_id):
-                     print(" You are already have account .")
+                if row["account_id"] == account_id:
+                    print("You already have an account.")
+                    return  
+        
+        with open('bank.csv', 'a+', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+            writer.writerow({'account_id': account_id, 'first_name': first_name, 'last_name': last_name, 'password': password, 'balance_checking': balance_checking, 'balance_savings': balance_savings})
+            print("added account successfully.")
+
+    def login(self):
+        account_id = input("Enter yoyr ID: ")
+        password = input("Enter password: ")
+        
+        with open('bank.csv', "r", newline="") as file:
+            contents = csv.DictReader(file)
+            for row in contents:
+                if row["account_id"] == account_id and row["password"] == password:
+                    print("Welcome!")
+                    self.is_logged_in = True  
+                    self.logged_in_user_id = account_id
+                    return
+                
+        print("Invalid ID or password! Please try again.")
+
+    def logout(self):
+        if self.is_logged_in:
+            print("Goodbye !")
+            self.is_logged_in = False 
     
-         with open('bank.csv', 'a+', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({'account_id': account_id,'first_name': first_name,'last_name': last_name,'password': password,'balance_checking': balance_checking,'balance_savings': balance_savings})
-            print(f"Customer {first_name} added successfully.")
-    
-    def login():
+    def display_info(self):
+        if self.is_logged_in:
+            with open('bank.csv', "r", newline="") as file:
+                contents = csv.DictReader(file)
+                for row in contents:
+                    if row["account_id"] == self.logged_in_user_id:
+                        print("Account Information:")
+                        print(f"ID: {row['account_id']}")
+                        print(f"Name: {row['first_name']} {row['last_name']}")
+                        print(f"Checking Balance: {row['balance_checking']}")
+                        print(f"Savings Balance: {row['balance_savings']}")
+                        return 
+        else:
+            print("Please log in first.")
+            
+            
+class bankAccount:
+    def deposit():
         pass
     
-    def logout():
+    def withdraw():
         pass
     
-    def authenticate():
-        pass 
+    def transfer():
+        pass  
     
-    
-cust = bankAccount('1009', 'ruba', 'alharbi', 123321)
-cust.addAccount('1009', 'ruba', 'alharbi', 123321, 0, 0)
+class checkingAccount(bankAccount):
+    pass
+
+class savingAccount(bankAccount):
+    pass
+
+class history:
+    pass
+
+
+#--------------------main------------------#
+
+# Usage
+# bank_account = bank("", "", "", "")
+# bank_account.addAccount()
+bank_account = bank()
+bank_account.add_account()
+bank_account.login()
+bank_account.display_info()
